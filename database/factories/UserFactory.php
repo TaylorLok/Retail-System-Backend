a35b7441b6f33371f5b8791a332fddbce982ba61
Taylor <taylor@dwebserver.net>
2024-08-02 14:49:38 +0200

    feature/i added 1 usertype seeder admin and link this to the existing admin user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,12 +5,20 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\UserType;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
     /**
      * The current password being used by the factory.
      */
@@ -29,6 +37,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'user_type_id' => UserType::factory(),
         ];
     }
 
---
+++ database/factories/UserTypeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserType>
+ */
+class UserTypeFactory extends Factory
+{
+    protected $model = UserType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => 'Admin',
+            'description' => 'This is admin user',
+        ];
+    }
+}
